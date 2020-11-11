3d14956baa37572b66d5eb4ca851dd6b260b2dc6
utgwkk <utagawakiki@gmail.com>
2020-11-11 19:06:44 +0900

    minil new

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Perl::Critic::Policy::ProhibitOrReturn - It's new $module
+
+# SYNOPSIS
+
+    use Perl::Critic::Policy::ProhibitOrReturn;
+
+# DESCRIPTION
+
+Perl::Critic::Policy::ProhibitOrReturn is ...
+
+# LICENSE
+
+Copyright (C) utgwkk.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+utgwkk <utagawakiki@gmail.com>
---
+++ lib/Perl/Critic/Policy/ProhibitOrReturn.pm
@@ -0,0 +1,39 @@
+package Perl::Critic::Policy::ProhibitOrReturn;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Perl::Critic::Policy::ProhibitOrReturn - It's new $module
+
+=head1 SYNOPSIS
+
+    use Perl::Critic::Policy::ProhibitOrReturn;
+
+=head1 DESCRIPTION
+
+Perl::Critic::Policy::ProhibitOrReturn is ...
+
+=head1 LICENSE
+
+Copyright (C) utgwkk.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+utgwkk E<lt>utagawakiki@gmail.comE<gt>
+
+=cut
+
